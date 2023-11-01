576417a15845a431a3a85af03d5428ff235e73a1
Grygorii Gontarchuk <105290753+gregorygont@users.noreply.github.com>
2023-11-01 20:37:44 +0200

    fixed

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,7 +8,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer != "Bearer" || !token)
+  if (bearer !== "Bearer" || !token)
     return next(HttpError(401, "Not authorized"));
 
   try {
